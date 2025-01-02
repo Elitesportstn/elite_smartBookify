d6a0bf30ac719975c9e801e42c9e8e30385b6775
4li <ali@4li.4li>
2025-01-02 16:27:09 +0100

    update init pay body

--- spring-api/src/main/java/com/elite/app/builder/utils/InitPayRequest.java
+++ spring-api/src/main/java/com/elite/app/builder/utils/InitPayRequest.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
  @AllArgsConstructor
 @NoArgsConstructor
 public class InitPayRequest {
-    public  String eamil;
+    public  String email;
      public  String paymentRef;
 }
